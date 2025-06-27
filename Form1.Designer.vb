0e8839c9387507533132314baced2e10a470acac
muziiiq <phytonz1807@gmail.com>
2025-06-27 13:13:17 +0800

    Login Page

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,30 +22,123 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Button1 = New Button()
+        PictureBox1 = New PictureBox()
+        Panel1 = New Panel()
+        txtUsername = New TextBox()
+        txtPassword = New TextBox()
+        btnLogIn = New Button()
+        TextBox1 = New TextBox()
+        btnSignIn = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        Panel1.SuspendLayout()
         SuspendLayout()
         ' 
-        ' Button1
+        ' PictureBox1
         ' 
-        Button1.Location = New Point(364, 192)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(94, 29)
-        Button1.TabIndex = 0
-        Button1.Text = "Button1"
-        Button1.UseVisualStyleBackColor = True
+        PictureBox1.Image = My.Resources.Resources.WhatsApp_Image_2025_05_21_at_18_24_37_f09943ca
+        PictureBox1.Location = New Point(511, 40)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(287, 272)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(PictureBox1)
+        Panel1.Dock = DockStyle.Top
+        Panel1.Location = New Point(0, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(1366, 343)
+        Panel1.TabIndex = 1
+        ' 
+        ' txtUsername
+        ' 
+        txtUsername.BorderStyle = BorderStyle.FixedSingle
+        txtUsername.Cursor = Cursors.IBeam
+        txtUsername.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtUsername.Location = New Point(457, 349)
+        txtUsername.Name = "txtUsername"
+        txtUsername.PlaceholderText = "                                        USERNAME"
+        txtUsername.Size = New Size(401, 27)
+        txtUsername.TabIndex = 2
+        ' 
+        ' txtPassword
+        ' 
+        txtPassword.BorderStyle = BorderStyle.FixedSingle
+        txtPassword.Cursor = Cursors.IBeam
+        txtPassword.Location = New Point(457, 403)
+        txtPassword.Name = "txtPassword"
+        txtPassword.PlaceholderText = "                                        PASSWORD"
+        txtPassword.Size = New Size(401, 27)
+        txtPassword.TabIndex = 3
+        ' 
+        ' btnLogIn
+        ' 
+        btnLogIn.BackColor = Color.FromArgb(CByte(38), CByte(222), CByte(129))
+        btnLogIn.FlatAppearance.BorderSize = 0
+        btnLogIn.FlatAppearance.MouseOverBackColor = Color.FromArgb(CByte(33), CByte(140), CByte(116))
+        btnLogIn.FlatStyle = FlatStyle.Popup
+        btnLogIn.Font = New Font("Calibri", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnLogIn.Location = New Point(457, 519)
+        btnLogIn.Name = "btnLogIn"
+        btnLogIn.Size = New Size(401, 50)
+        btnLogIn.TabIndex = 4
+        btnLogIn.Text = "Log In"
+        btnLogIn.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BorderStyle = BorderStyle.FixedSingle
+        TextBox1.Cursor = Cursors.IBeam
+        TextBox1.Location = New Point(457, 464)
+        TextBox1.Name = "TextBox1"
+        TextBox1.PlaceholderText = "                                     ADMIN PASSKEY"
+        TextBox1.Size = New Size(401, 27)
+        TextBox1.TabIndex = 5
+        ' 
+        ' btnSignIn
+        ' 
+        btnSignIn.BackColor = Color.FromArgb(CByte(210), CByte(227), CByte(188))
+        btnSignIn.FlatAppearance.BorderColor = Color.FromArgb(CByte(192), CByte(255), CByte(255))
+        btnSignIn.FlatAppearance.BorderSize = 0
+        btnSignIn.FlatStyle = FlatStyle.Flat
+        btnSignIn.Font = New Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnSignIn.Location = New Point(521, 595)
+        btnSignIn.Name = "btnSignIn"
+        btnSignIn.Size = New Size(287, 29)
+        btnSignIn.TabIndex = 6
+        btnSignIn.Text = "Don't Have an Account? Sign in Here"
+        btnSignIn.UseVisualStyleBackColor = False
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(210), CByte(227), CByte(188))
         ClientSize = New Size(1366, 768)
-        Controls.Add(Button1)
+        Controls.Add(btnSignIn)
+        Controls.Add(TextBox1)
+        Controls.Add(btnLogIn)
+        Controls.Add(txtPassword)
+        Controls.Add(txtUsername)
+        Controls.Add(Panel1)
         MaximizeBox = False
         Name = "Form1"
-        Text = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Login Page"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        Panel1.ResumeLayout(False)
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnLogIn As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnSignIn As Button
 
 End Class
